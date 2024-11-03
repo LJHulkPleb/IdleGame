e0b84569d974dd5b953fbacd266f674f8a4e58af
Coelor <coelortv@gmail.com>
2024-11-03 10:39:56 -0600

    Made some small changes to menus. Disables FPSController script now Open Menu with E (Less intrusive)

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,58 +1,115 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class MenuManager : MonoBehaviour
 {
     public GameObject farmMenu;
     public GameObject petMenu;
+    public GameObject FpsController;
     public Camera playerCamera;
     public float detectionRange = 5f;
+    public TMP_Text ActionText;
+
+    private bool m_IsMenuOpen = false;
+    private string m_CurrentTag;
+    private FirstPersonController M_FpsControllerScript;
+
 
     private void Start()
     {
         farmMenu.SetActive(false);
         petMenu.SetActive(false);
+        ActionText.gameObject.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        M_FpsControllerScript = FpsController.GetComponent<FirstPersonController>();
     }
 
     private void Update()
     {
-        DetectObject();
+        if (!m_IsMenuOpen)
+            DetectObject();
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (m_IsMenuOpen)
+                CloseMenu();
+            else
+                OpenMenuForCurrentTag();
+        }
     }
 
-    void DetectObject()
+    private void DetectObject()
     {
-        Ray ray = playerCamera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));
+        var ray = playerCamera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));
         RaycastHit hit;
-        if(Physics.Raycast(ray, out hit, detectionRange))
+
+        if (Physics.Raycast(ray, out hit, detectionRange))
         {
-            if (hit.collider.CompareTag("Farm"))
+            var tag = hit.collider.tag;
+
+            if (tag is "Farm" or "Pet")
             {
-                ShowFarmMenu();
-            }
-            else if (hit.collider.CompareTag("Pet")){
-                ShowPetMenu();
-            }
-            else
-            {
-                HideAllMenus();
+                m_CurrentTag = tag;
+                ActionText.text = "Press E to interact with " + tag;
+                ActionText.gameObject.SetActive(true);
             }
         }
         else
         {
-            HideAllMenus();
+            ActionText.gameObject.SetActive(false);
+            m_CurrentTag = "";
+            ActionText.text = "";
         }
     }
+
+    private void OpenMenuForCurrentTag()
+    {
+        if (m_CurrentTag == "Farm")
+        {
+            ShowFarmMenu();
+        }
+        else if (m_CurrentTag == "Pet")
+        {
+            ShowPetMenu();
+        }
+
+        if (!string.IsNullOrEmpty(m_CurrentTag))
+        {
+            m_IsMenuOpen = true;
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+
+            M_FpsControllerScript.enabled = false;
+        }
+    }
+
+
     void ShowFarmMenu()
     {
         farmMenu.SetActive(true);
+        ActionText.gameObject.SetActive(false);
         petMenu.SetActive(false);
     }
+
     void ShowPetMenu()
     {
         farmMenu.SetActive(false);
+        ActionText.gameObject.SetActive(false);
         petMenu.SetActive(true);
     }
+
+    void CloseMenu()
+    {
+        HideAllMenus();
+        m_IsMenuOpen = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        M_FpsControllerScript.enabled = true;
+    }
+
     void HideAllMenus()
     {
         farmMenu.SetActive(false);
--- Assets/Scripts/PetMenuManager.cs
+++ Assets/Scripts/PetMenuManager.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class PetMenuManager : MonoBehaviour
 {
     public Pet pet;
-    public Text StrengthText;
-    public Text DefenseText;
-    public Text HealthText;
+    public TMP_Text NameText;
+    public TMP_Text HealthText;
+    public TMP_Text StrengthText;
+    public TMP_Text DefenseText;
+    
     public void UpdateStatsUI()
     {
+        //NameText.text = pet.Name.ToString();
         StrengthText.text = "Strength: " + pet.Strength.ToString();
         DefenseText.text = "Defense: " + pet.Defense.ToString();
         HealthText.text = "Health: " + pet.Health.ToString();
